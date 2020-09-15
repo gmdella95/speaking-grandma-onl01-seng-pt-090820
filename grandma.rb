b7fa295a681f95dc579b2c05405bf956f15be4d4
Gustavo Dellacroce <gmdella95@gmail.com>
2020-09-15 02:02:44 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,11 +1,11 @@
 
 
 def speak_to_grandma(phrase)
-  if phrase == phrase.downcase
-    "HUH?! SPEAK UP SONNY!"
+  if phrase == "I love you grandma!".upcase
+    "I LOVE YOU TOO PUMPKIN!"
   elsif phrase == phrase.upcase
     "NO, NOT SINCE 1938!"
-  else phrase == "I love you grandma!".upcase
-    "I LOVE YOU TOO PUMPKIN!"
+  else phrase == phrase.downcase
+  "HUH?! SPEAK UP SONNY!"
   end
 end
